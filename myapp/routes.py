b3de5f0c1b4774b21c5a2b970f8c0cb5bebba804
mumi777 <109796361+mumi777@users.noreply.github.com>
2024-06-17 23:03:58 +0900

    ver1

---
+++ myapp/routes.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for, request
+from myapp import app
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch.nn.functional as F
+import random
+
+f = open("Good_Food.txt", 'r')
+Good = f.readline()
+list_Good = [Good]
+g = open("Bad_Food.txt", 'r')
+Bad = g.readline()
+list_Bad = [Bad]
+
+
+model_name = "kykim/bert-kor-base"
+model = BertForSequenceClassification.from_pretrained(model_name, num_labels=2)
+tokenizer = BertTokenizer.from_pretrained(model_name)
+
+def classify_emotion(text):
+    # 텍스트 토큰화, 패딩
+    tokens = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+
+    # 예측
+    with torch.no_grad():
+        prediction = model(**tokens)
+
+		# 감정 예측 및 출력
+    prediction = F.softmax(prediction.logits, dim=1)
+    output = prediction.argmax(dim=1).item()
+    labels = ["부정적", "긍정적"]
+    goose = labels[output]
+    if goose == "부정적":
+        ask = "저런... 기분이 안좋으시군요. {} 같은 음식은 어떠신가요?".format(random.choice(list_Bad))
+
+    elif goose == "긍정적":
+        ask = "좋은 일이 있으셨군요! {}은(는) 어떠신가요?".format(random.choice(list_Good))
+    return render_template(
+        "chat.html", title="Chat", 
+    )
+@app.route("/")
+@app.route("/index")
+def index_func():
+    return render_template("index.html", title="Home")
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat_func():
+    data = request.get_json()
+    sentence = data["text"]
+    result = classify_emotion(sentence)
+    return render_template("chat.html", title="Chat")
