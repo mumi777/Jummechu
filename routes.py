326ff9b8bb6dca7505df7b5ee4b2e84697e91c54
mumi777 <109796361+mumi777@users.noreply.github.com>
2024-07-09 22:31:01 +0900

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import torch
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch.nn.functional as F
