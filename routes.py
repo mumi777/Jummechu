ed5e78758424a208987aadcbcc39eac2c07c4d88
mumi777 <109796361+mumi777@users.noreply.github.com>
2024-07-09 21:19:55 +0900

    routes.py 업데이트

--- routes.py
+++ routes.py
@@ -9,47 +9,7 @@ import os
 app = Flask(__name__)
 CORS(app)  # CORS 설정 추가
 
-script_dir = os.path.dirname(__file__)
-file_path_good = os.path.join(script_dir, 'Good_Food.txt')
-file_path_bad = os.path.join(script_dir, 'Bad_Food.txt')
 
-try:
-    with open(file_path_good, 'r') as file:
-        Goods = file.read().splitlines()
-        print("Good_Food.txt 파일을 성공적으로 읽었습니다.")
-except FileNotFoundError:
-    print(f"파일을 찾을 수 없습니다: {file_path_good}")
-except Exception as e:
-    print(f"오류가 발생했습니다: {e}")
-
-try:
-    with open(file_path_bad, 'r') as file:
-        Bads = file.read().splitlines()
-        print("Bad_Food.txt 파일을 성공적으로 읽었습니다.")
-except FileNotFoundError:
-    print(f"파일을 찾을 수 없습니다: {file_path_bad}")
-except Exception as e:
-    print(f"오류가 발생했습니다: {e}")
-
-model_name = "kykim/bert-kor-base"
-model = BertForSequenceClassification.from_pretrained(model_name, num_labels=2)
-tokenizer = BertTokenizer.from_pretrained(model_name)
-
-def classify_emotion(text):
-    tokens = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
-    with torch.no_grad():
-        prediction = model(**tokens)
-    prediction = F.softmax(prediction.logits, dim=1)
-    output = prediction.argmax(dim=1).item()
-    labels = ["부정적", "긍정적"]
-    emotion = labels[output]
-
-    if emotion == "부정적":
-        response = "저런... 기분이 안 좋으시군요. {} 같은 음식은 어떠신가요?".format(random.choice(Bads))
-    else:
-        response = "좋은 일이 있으셨군요! {}은(는) 어떠신가요?".format(random.choice(Goods))
-
-    return emotion, response
 
 @app.route("/")
 def index_func():
